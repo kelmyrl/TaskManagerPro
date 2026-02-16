803c40fecf903e8fe81add77bc751099171009ef
kelmyrl <betykelmi@gmail.com>
2026-02-16 11:33:27 -0600

    Add SimpleTask subclass

---
+++ SimpleTask.cs
@@ -0,0 +1,13 @@
+namespace TaskManagerPro;
+
+public class SimpleTask : BaseTask
+{
+    public SimpleTask(string title) : base(title)
+    {
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine($"[Simple] {Title} - Completed : {IsCompleted}");
+    }
+}
