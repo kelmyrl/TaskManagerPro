3f6976392cc43f0987f17329f4ac9c1cd19ff339
kelmyrl <betykelmi@gmail.com>
2026-02-18 11:02:32 -0600

    Add Program.cs file using Polymorphism

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using TaskManagerPro;
+
+var tasks = new List<ITask>
+{
+    new SimpleTask("Buy groceries"),
+    new SimpleTask("Email Professor")
+};
+
+foreach (var task in tasks)
+{
+    task.Display();
+}
+
+tasks[0].Complete();
+
+Console.WriteLine("\nAfter completing the first task:\n");
+
+foreach (var task in tasks)
+{
+    task.Display();
+}
